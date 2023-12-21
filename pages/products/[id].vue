453cfd9c29f6cecd317ef3e762967603b12dc1e9
markojp <joakim.forthewin@outlook.com>
2023-12-21 23:51:48 +0100

    fetch data from api, show image with nuxt-img

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <p>Product detail for {{ id }}</p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est asperiores ea
-      commodi libero. Reprehenderit iure voluptatum, nulla officia optio veniam.
-    </p>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
+    <nuxt-img :src="product.image" />
   </div>
 </template>
 
 <script setup>
 definePageMeta({ layout: "products" });
 const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+//width="100" height="150"
+const { data: product } = await useFetch(uri);
 </script>
 
 <style lang="scss" scoped></style>
